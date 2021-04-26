65bce1c6cb1af4a69b4c6325a714600e2aac68b3
jenropski <63664056+jenropski@users.noreply.github.com>
2021-04-25 21:38:44 -0400

    omg it worked

--- dev/js/zoomIn.js
+++ dev/js/zoomIn.js
@@ -2,17 +2,13 @@ import { gsap } from "gsap";
 
 export let zoomTL = gsap.timeline();
 
-gsap.set("#orange-mtn",{transformOrigin:"center"});
-gsap.set("#red-mtn",{transformOrigin:"center"});
-gsap.set(".front-mtns",{transformOrigin:"center"});
-gsap.set(".trees",{transformOrigin:"center"});
+gsap.set("#background",{transformOrigin:"center"});
 
 
-zoomTL.from("#orange-mtn",{duration:6, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
-            .from("#red-mtn",{duration:5.75, scale:10,ease: "power4.out",y:"+=800"},"zoom")
-            .from(".front-mtns",{duration:5.5, scale:10,ease: "power4.out",y:"+=800"},"zoom")
-            .from("#trees-5",{duration:5.5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
-            .from("#trees-4",{duration:5.25, scale:10,ease: "power4.out",y:"+=420"},"zoom")
-            .from("#trees-3",{duration:5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
-            .from("#trees-2",{duration:5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
-            .from("#trees-1",{duration:4.75, scale:10,ease: "power4.out",y:"+=420"},"zoom")
+zoomTL.from("#ufo",{duration:3, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
+            .from("#background",{duration:5.75, scale:10,ease: "power4.out",y:"+=800"},"zoom")
+            .from("#cactus1",{duration:5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
+            .from("#cactus2",{duration:5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
+            .from("#cactus3",{duration:4.75, scale:10,ease: "power4.out",y:"+=420"},"zoom")
+            .from("#mountains",{duration:4.75, scale:10,ease: "power4.out",y:"+=420"},"zoom")
+            .from("#spaceship",{duration:4.75, scale:10,ease: "power4.out",y:"+=420"},"zoom")
