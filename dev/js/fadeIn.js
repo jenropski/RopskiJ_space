2612c27be7b42b7781234ebdcc0c2964a7fb4cca
jenropski <63664056+jenropski@users.noreply.github.com>
2021-04-26 11:17:39 -0400

    publish to github

--- dev/js/fadeIn.js
+++ dev/js/fadeIn.js
@@ -2,4 +2,4 @@ import { gsap } from "gsap";
 
 export let fadeInTL = gsap.timeline();
 fadeInTL.from("#background",{alpha:0, duration:3, scale:20})
-        .from("#background",{alpha:0, stagger:0.25, duration:0.5},"-=2");
+        .from("#",{alpha:0, stagger:0.25, duration:0.5},"-=2");
